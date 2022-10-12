0974425ee71ff0d7db0592955cde545de7e6f623
hbollon <hugo.bollon@gmail.com>
2022-10-12 10:49:29 +0200

    chore: improved startup script

--- launch.sh
+++ launch.sh
@@ -2,10 +2,10 @@
 
 mkdir /etc/blackbox_exporter/
 
-rm /etc/blackbox_exporter/config_web.yml || :
+rm -f /etc/blackbox_exporter/config.yml || :
 envsubst < config.yml > /etc/blackbox_exporter/config.yml
 
-rm /etc/blackbox_exporter/config_web.yml || :
+rm -f /etc/blackbox_exporter/config_web.yml || :
 envsubst < config_web.yml > /etc/blackbox_exporter/config_web.yml
 
 /bin/blackbox_exporter --config.file=/etc/blackbox_exporter/config.yml --web.config.file=/etc/blackbox_exporter/config_web.yml
