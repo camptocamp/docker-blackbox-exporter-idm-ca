cf45a5658bc95caa6803c028c83f05895d65cade
Hugo Bollon <hugo.bollon@camptocamp.com>
2023-09-18 12:03:41 +0200

    feat: add env variable to set log-level

--- launch.sh
+++ launch.sh
@@ -8,4 +8,10 @@ envsubst < config.yml > /etc/blackbox_exporter/config.yml
 rm -f /etc/blackbox_exporter/config_web.yml || :
 envsubst < config_web.yml > /etc/blackbox_exporter/config_web.yml
 
-/bin/blackbox_exporter --config.file=/etc/blackbox_exporter/config.yml --web.config.file=/etc/blackbox_exporter/config_web.yml
+if [[ -z "${APP_LOG_LEVEL}" ]]; then
+  LOG_LEVEL="info"
+else
+  LOG_LEVEL="${APP_LOG_LEVEL}"
+fi
+
+/bin/blackbox_exporter --log.level=${LOG_LEVEL} --config.file=/etc/blackbox_exporter/config.yml --web.config.file=/etc/blackbox_exporter/config_web.yml
