09e3b8d6d3da1f9b5d25d90817b60e9d008ba91b
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2024-04-24 11:27:31 +0200

    feat: enable prober logs

--- launch.sh
+++ launch.sh
@@ -10,8 +10,10 @@ envsubst < config_web.yml > /etc/blackbox_exporter/config_web.yml
 
 if [[ -z "${APP_LOG_LEVEL}" ]]; then
   LOG_LEVEL="info"
+  LOG_PROBER="info"
 else
   LOG_LEVEL="${APP_LOG_LEVEL}"
+  LOG_PROBER="${APP_LOG_LEVEL}"
 fi
 
-/bin/blackbox_exporter --log.level=${LOG_LEVEL} --config.file=/etc/blackbox_exporter/config.yml --web.config.file=/etc/blackbox_exporter/config_web.yml
+/bin/blackbox_exporter --log.level=${LOG_LEVEL} --log.prober=${LOG_PROBER} --config.file=/etc/blackbox_exporter/config.yml --web.config.file=/etc/blackbox_exporter/config_web.yml
